590be6e62857b3d56e86f9366e4eded0ef2b14cd
Edema <edemaero@gmail.com>
2022-09-24 16:20:08 +0100

    update

--- pages/api/paystack.js
+++ pages/api/paystack.js
@@ -4,16 +4,17 @@ import React from "react";
 import { PaystackButton } from "react-paystack";
 import { useStateContext } from "../../context/StateContext";
 
-
 const PayStack = () => {
-  const { name, email, validateEmail, componentProps } = useStateContext();
+  const { validateEmail, componentProps } = useStateContext();
+  const firstName = sessionStorage.getItem("firstName");
+  const email = sessionStorage.getItem("email");
 
   return (
     <>
       <PaystackButton
         {...componentProps}
         className={`btn ${
-          name.length < 1 || !validateEmail(email) ? "btn-disabled" : ""
+          firstName || validateEmail(email) ? "btn-disabled" : ""
         }`}
       />
     </>
